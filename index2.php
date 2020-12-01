c02fcfeda275088e0264f8450203811d86f1cade
jgcarvajal <jgcarvajalh@uqvirtual.edu.co>
2020-11-30 22:53:39 -0500

    Correcion relaciones con php

--- index2.php
+++ index2.php
@@ -5,26 +5,37 @@ $celular = $_GET['numero_cel'];
 $telefono = $_GET['numero_tel'];
 $otro = $_GET['otro'];
 $listar = $_GET['listar'];
+$agregar = $_GET['crear'];
 $borrar = $_GET['Borrar'];
 
 
 echo "<h2>Informacion de los usuarios</h2>";
+
+if ($_GET['function']=="editar") {
+   prueba();
+ }
+ elseif ($_GET['function']=="listar") {
+   listar();
+ }
 #echo "El Nombre del Usuario es:". $nombre ."<br/>";
 #echo "El Numero de Habitacion es:". $habitacion ."<br/>";
 #echo "El Numero de Celular es:". $celular ."<br/>";
 #echo "El Numero de Telefono es:". $telefono ."<br/>";
 #echo "Otro Numero de Telefono es:". $otro ."<br/>";
-
-exec ('/bin/bash -c "getent passwd {1000..2000}"', $listar );
-echo "<pre>";
-foreach ( $listar as $linea )
-{
-   echo $linea."\n";
+function listar(){
+   exec ('/bin/bash -c "getent passwd {1000..2000}"', $listar );
+   echo "<pre>";
+   foreach ( $listar as $linea )
+   {
+      echo $linea."\n";
+   }
+   echo "</pre>";
 }
-echo "</pre>";
-
-
-exec ('sudo userdel 
-)
+function prueba(){
+   echo "pruebassss";
+}
 
 ?>
+<form action="index.html" method="get">
+<p><input type="submit" value="Regresar"/></p>
+</form>
